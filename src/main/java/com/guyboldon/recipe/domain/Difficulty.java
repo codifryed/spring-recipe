16fde87b95e751130af41a1f8644242a703f80e5
Guy Boldon <gvboldon@gmail.com>
2019-01-21 19:20:40 +0100

    Add Difficulty enum with relationship

---
+++ src/main/java/com/guyboldon/recipe/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.guyboldon.recipe.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- src/main/java/com/guyboldon/recipe/domain/Recipe.java
+++ src/main/java/com/guyboldon/recipe/domain/Recipe.java
@@ -17,8 +17,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // TODO: add
-    //  private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
@@ -117,4 +118,11 @@ public class Recipe {
         this.notes = notes;
     }
 
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
 }
