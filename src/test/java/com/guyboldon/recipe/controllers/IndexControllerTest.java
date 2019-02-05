7c5018d3e55d9b0ce734474e1131417f324bb516
Guy Boldon <guy.boldon@initia-medien.de>
2019-02-05 23:30:32 +0100

    Add indexController unit test with Mockito

---
+++ src/test/java/com/guyboldon/recipe/controllers/IndexControllerTest.java
@@ -0,0 +1,60 @@
+package com.guyboldon.recipe.controllers;
+
+import com.guyboldon.recipe.domain.Recipe;
+import com.guyboldon.recipe.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class IndexControllerTest {
+
+    IndexController indexController;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+        //given:
+        Recipe recipe1 = new Recipe()
+                .setDescription("recipe1");
+        Recipe recipe2 = new Recipe()
+                .setDescription("recipe2");
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(recipe1);
+        recipes.add(recipe2);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when:
+        String returnedValue = indexController.getIndexPage(model);
+
+        //then:
+        assertEquals("index", returnedValue);
+//        assertTrue(model.containsAttribute("recipes"));
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        verify(recipeService, times(1)).getRecipes();
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
+    }
+}
